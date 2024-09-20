07730e9e9753956fd1d058ab4f4d0c9c6c0fd919
Akhilnath G <akhillinkedin1@gmail.com>
2024-09-21 00:44:45 +0530

    commiting 3 sample python pgms to main

---
+++ Python/sum dynamic.py
@@ -0,0 +1,3 @@
+a=int(input("Enter 1st num:\t"))
+b=int(input("Enter 2nd num:\t24"))
+print("Sum = ",a+b)
---
+++ Python/sum_funct.py
@@ -0,0 +1,5 @@
+def sum():
+  a=int(input("Enter 1st num:\t"))
+  b=int(input("Enter 2nd num:\t"))
+  print("Sum = ",a+b)
+sum()  
